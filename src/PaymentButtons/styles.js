62af225fe19ea215b8bdc94a75b3a8cb0446e8dc
Oriana <oriabreu.v@gmail.com>
2021-02-26 14:27:48 +0100

    change button hover color

--- src/PaymentButtons/styles.js
+++ src/PaymentButtons/styles.js
@@ -9,6 +9,10 @@ const useStyles = makeStyles((theme) => ({
       backgroundColor: theme.palette.primary.main,
       borderColor: theme.palette.primary.main,
       color: '#fff',
+      '&:hover': {
+        backgroundColor: theme.palette.primary.dark,
+        borderColor: theme.palette.primary.dark,
+      }
     },
   }));
 
