46fb640beaf515e908c9b5b211e5cbd07c5fce7b
Oriana <oriabreu.v@gmail.com>
2021-02-28 23:30:22 +0100

    linter config

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+    env: {
+      es6: true,
+      jest: true,
+      browser: true
+    },
+    extends: ["airbnb", "prettier", "prettier/react"],
+    globals: {
+      Atomics: "readonly",
+      SharedArrayBuffer: "readonly",
+      _DEV_: true
+    },
+    parser: "babel-eslint",
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true
+      },
+      ecmaVersion: 2018,
+      sourceType: "module"
+    },
+    plugins: ["react", "jsx-a11y", "import", "react-hooks", "prettier"],
+    rules: {
+      "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx"] }],
+      "react/jsx-one-expression-per-line": "off",
+      "react/jsx-props-no-spreading": "off",
+      "react/forbid-prop-types": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "error",
+      "no-console": ["error", { allow: ["tron"] }],
+      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      "no-param-reassign": "off",
+      camelcase: "warn",
+      "no-underscore-dangle": "off",
+      "import/no-cycle": "off",
+      "prettier/prettier": "error",
+      "import/prefer-default-export": "off",
+      "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+      "jsx-a11y/label-has-for": "off",
+      "jsx-a11y/label-has-associated-control": "off",
+      "jsx-a11y/mouse-events-have-key-events": "off"
+    },
+    settings: {
+      "import/resolver": {
+        "babel-plugin-root-import": {
+          rootPathSuffix: "src"
+        }
+      }
+    }
+  };
